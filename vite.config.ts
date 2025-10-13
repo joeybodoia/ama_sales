97cb07b465bfbd88e8d71e83593ce91042345ec2
joeybodoia <jbodoia21@cmc.edu>
2025-10-13 14:01:51 -0700

    Fix React refresh and HMR WebSocket errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: 'automatic' })],
   server: {
+    hmr: {
+      protocol: 'wss',
+      host: true,
+      clientPort: 443,
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
